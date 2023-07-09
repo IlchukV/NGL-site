58fba45898bf267300131c35316bdb6ecfea26c4
Vadim Ilchuk <vadmany@gmail.com>
2023-07-09 14:48:39 +0200

    14

--- src/js/translate.js
+++ src/js/translate.js
@@ -1,20 +1,22 @@
-const select = document.querySelector(`select`);
-const allLang = ["en", "ua", "ru"];
+// const select = document.querySelector(`select`);
+// const allLang = ["en", "ua", "ru"];
 
-select.addEventListener(`change`, changeURLlanguage);
-function changeURLlanguage() {
-    let leng = select.value;
-    location.href = window.location.pathname + `#` + lang;
-    location.reload();
-}
+// select.addEventListener(`change`, changeURLlanguage);
+// function changeURLlanguage() {
+//     let leng = select.value;
+//     location.href = window.location.pathname + `#` + lang;
+//     location.reload();
+// }
 
-function changeLanguage() {
-    let hash = window.location.hash;
-    hash = hash.substr(1);
-    if (!allLang.includes(hash)) {
-        location.href = window.location.pathname + `#ru`;
-        location.reload();
-    }
-    select.value = hash;
-    document.querySelector
-}
+// function changeLanguage() {
+//     let hash = window.location.hash;
+//     // hash = hash.substr(1);
+//     console.log(hash)
+//     if (!allLang.includes(hash)) {
+//         location.href = window.location.pathname + `#ru`;
+//         location.reload();
+//     }
+//     select.value = hash;
+//     document.querySelector
+// }
+// changeLanguage()
