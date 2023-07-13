94524ec6ef52f9e3cd5f36137bca87bd0218f2a4
Vadim Ilchuk <vadmany@gmail.com>
2023-07-13 22:39:07 +0200

    34

--- src/js/loader.js
+++ src/js/loader.js
@@ -3,7 +3,7 @@ document.body.onload = function () {
     var preloader = document.getElementById('page-preloader');
     if (!preloader.classList.contains('done'))
     {
-        preloader.classList.add('done')
+        preloader.classList.add('done');
         }
 }, 3500)
 }
