fc67b407378a071ead98652a3c4df0b3d878671d
IlchukV <110021062+IlchukV@users.noreply.github.com>
2023-07-15 17:28:41 +0000

    Deploying to gh-pages from @ IlchukV/NGL-site@5621d45fd8807e7bb0d54a49f6ae77d75e3717bb 🚀

--- index.08f0ae49.js
+++
@@ -1,2 +0,0 @@
-document.body.onload=function(){setTimeout((function(){var e=document.getElementById("page-preloader");e.classList.contains("done")||e.classList.add("done")}),3500)};
-//# sourceMappingURL=index.08f0ae49.js.map
--- index.54ea145e.js
+++
@@ -1,2 +0,0 @@
-document.body.onload=function(){setTimeout((function(){var e=document.getElementById("page-preloader");e.classList.contains("done")||e.classList.add("done")}),3500)};
-//# sourceMappingURL=index.54ea145e.js.map
